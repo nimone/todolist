ec986c386f4b8885fb496f2c8d36239950dfbd91
nimone <nishantmogha1234@gmail.com>
2021-11-02 15:25:34 +0530

    refactor Select component usage & add project selection to ProjectHeader

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,36 +1,37 @@
 import React, { useState } from 'react'
 import { ChevronDown } from 'react-feather'
 
-function Select({ options, selected, onSelect }) {
+function Select({ text, className, children }) {
 	const [showOptions, setShowOptions] = useState(false)
-	
-	const handleSelect = value => {
-		onSelect(value)
-		setShowOptions(false)
-	}
 
 	return (
 	  <div className="relative">
 	    <button 
-	    	className="relative flex items-center space-x-1 bg-transparent focus:(outline-none)" 
+	    	className={`relative flex items-center space-x-1 bg-transparent focus:(outline-none) ${className}`} 
 	    	onClick={() => setShowOptions(prev => !prev)}>
-	    	<span>{options[selected]}</span> 
+	    	<span>{text}</span> 
 		    <ChevronDown />
 	    </button>
+	    
 			{showOptions && 
-				<ul className="absolute z-10 mt-1 w-full bg-gray-900/60 text-white shadow-lg max-h-56 rounded-b py-1 overflow-auto focus:outline-none">
-					{Object.entries(options).map(([ value, text ]) => 
-				  	<li 
-				  		key={value}
-				  		className={`px-2 py-1 hover:bg-gray-900/70 cursor-pointer ${value === selected ? "bg-gray-900/30" : ""}`}
-				  		onClick={() => handleSelect(value)}>
-							{text}
-				  	</li>
-					)}
+				<ul className="absolute z-10 mt-2 w-full bg-gray-900/70 text-white shadow-lg max-h-[70vh] max-w-64 rounded-b py-1 overflow-auto focus:outline-none"
+					onClick={() => setShowOptions(false)}
+				>
+					{children}
 				</ul>
 			}
-	    
 	  </div>
+	)
+}
+
+export function Option({ text, selected, ...props }) {
+	return (
+		<li 
+  		className={`px-2 py-1 hover:bg-gray-900/90 cursor-pointer ${selected ? "bg-gray-900/30" : ""}`}
+  		{...props}
+  	>
+			{text}
+  	</li>
 	)
 }
 export default Select
--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -2,22 +2,44 @@ import React from 'react'
 import { List, Settings } from "react-feather"
 import { useDispatch, useSelector } from 'react-redux'
 
-import { setSortType, sortOptions } from '../redux'
-import Select from './Select'
+import { setSortType, sortOptions, setCurrentProject } from '../redux'
+import Select, { Option } from './Select'
 
 function TodoHeader({ title, toggleSettings }) {
-  const sortType = useSelector(state => state.settings.sortType)
+  const { sortType, currentProject } = useSelector(state => state.settings)
+  const projects = useSelector(state => state.projects)
   const dispatch = useDispatch()
 
 	return (
     <div className="flex w-full justify-between items-center py-2 px-3 text-white bg-gradient-to-b from-gray-800 to-gray-900 opacity-80 rounded-t shadow-md">
       <List className="mr-3" />
-      <h1 className="font-bold mr-auto">{title}</h1>
-      <Select 
-        options={sortOptions} 
-        selected={sortType}
-        onSelect={value => dispatch(setSortType(value))}
-      />
+      <div className="mr-auto">
+        <Select 
+          text={projects[currentProject].title}
+          className="font-bold"
+        > {projects.map((project, idx) => (
+            <Option 
+              key={project.id}
+              selected={idx === currentProject}
+              onClick={() => dispatch(setCurrentProject(idx))}
+              value={idx}
+              text={project.title}
+            />
+          ))}
+        </Select>
+      </div>
+
+      <Select text={sortOptions[sortType]}>
+        {Object.entries(sortOptions).map(([ value, text ]) => (
+          <Option 
+            key={value}
+            selected={value === sortType}
+            onClick={() => dispatch(setSortType(value))}
+            value={value}
+            text={text}
+          />
+        ))}
+      </Select>
       <Settings 
         className="w-6 h-6 ml-3 cursor-pointer animated active:animate-spin" 
         onClick={toggleSettings}
