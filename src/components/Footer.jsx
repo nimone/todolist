f57c12e0b6711a436f70af4598efc0b69c4742c0
nimone <nishantmogha1234@gmail.com>
2021-12-18 10:02:41 +0530

    new less pronounced footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { GitHub, CheckCircle } from "react-feather"
+import React from "react"
+import { CheckCircle } from "react-feather"
 
 export default function Footer() {
-	return (
-		<footer className="absolute bottom-0 w-full flex justify-center items-center p-1 bg-gradient-to-t from-gray-900/50 to-gray-700/40 text-white">
-		 	<a href="https://github.com/nimone/todolist"
-		 		className="flex items-center mx-2"
-		 		target="_blank">
-			<CheckCircle className="w-5 h-5 text-violet-200" />
-		 	</a>
-			<span>
-				TodoList / Google Tasks client by
-			</span>
-			<a href="http://nimo.pages.dev"
-				className="border-b-2 border-green-300 ml-1" 
-				target="_blank">nimo</a> 
-		</footer>
-	)
+  return (
+    <footer
+      className={`
+			flex items-center
+			absolute bottom-3 right-3
+			bg-gray-800/70 text-sm text-white
+			p-2 rounded-lg
+		`}
+    >
+      <a
+        href="https://github.com/nimone/todolist"
+        target="_blank"
+        className="flex items-center"
+      >
+        <CheckCircle className="w-5 h-5 text-violet-300" />
+      </a>
+      <span className="mx-1">Google Tasks client by</span>
+      <a
+        href="https://nimo.pages.dev/"
+        target="_blank"
+        className="border-b-2 border-green-300"
+      >
+        nimo
+      </a>
+    </footer>
+  )
 }
